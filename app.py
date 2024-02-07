6d8b4c6d2415cafa20d2c52de68071f2595ce304
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2024-02-06 17:26:50 -0800

    updating app

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template
+
+import re
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+connection_string = "mongodb+srv://hangodb:hangodb@cluster0.phdgtft.mongodb.net/"
+dbname = "Hango"
+collection_name = "User Data"
+
+# Establish a connection to MongoDB Atlas
+client = MongoClient(connection_string)
+
+# Accessing the database
+db = client[dbname]
+
+# Accessing the collection
+collection = db[collection_name]
 
 app = Flask(__name__)
 
-#routes
-from user import routes
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('email')
+        password = request.form.get('password')
+
+        user = collection.find_one({'username': username, 'password': password})
+
+        if user: 
+            session['user'] = user
+            flash('Login Successful! :)', 'success')
+            # return redirect(url_for) -- redirect to main page aka where chatbox is 
+        else:
+            flash('Invalid username or password. Try again..')
     return render_template('login.html')

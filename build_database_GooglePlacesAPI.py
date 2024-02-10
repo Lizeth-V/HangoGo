224f13ad1ad349499dc3f0920f12ec6ed0e924e1
aidan-ma316 <77308659+aidan-ma316@users.noreply.github.com>
2024-02-10 15:27:37 -0800

    Update build_database_GooglePlacesAPI.py
    MongoDB part added! At the very end

--- build_database_GooglePlacesAPI.py
+++ build_database_GooglePlacesAPI.py
@@ -3,6 +3,7 @@ import googlemaps
 import pandas as pd
 import time
 
+
 api_key = "AIzaSyDHkwv5g7hUxpT8YS2MfnxJguUc87babIc"
 #base_url = "https://places.googleapis.com/v1/places:searchNearby"
 
@@ -10,32 +11,32 @@ map_client = googlemaps.Client(api_key)
 
 food_to_add = [
     #Food Categories
-    'Food',
-    'Dining',
-    'Bakery',]
+    'food',
+    'dining',
+    'bakery',]
 entertainment_to_add = [    
     #Entertainment Categories
-    'Entertainment',
-    'Arcade']
+    'entertainment',
+    'arcade']
 nature_rec_to_add = [
     #Nature/Recreation Categories
-    'Nature',
-    'Hike',
-    'Parks',
+    'nature',
+    'hike',
+    'parks',
 ]
 nightlife_to_add = [
     #Nightlife Categories
-    'Bar',
-    'Club',]
+    'bar',
+    'club',]
 mus_art_to_add = [ 
     #Museum/Art Categories
-    'Museum',
-    'Art',
-    'Library',]
+    'museum',
+    'art',
+    'library',]
 drinks_to_add = [    
     #Drinks Categories
-    'Cafe',
-    'Boba']
+    'cafe',
+    'boba']
 
 places_to_add = {
     'Food':food_to_add,
@@ -114,11 +115,15 @@ def mi_2_meters(miles):
 #This is measured in meters
 radius = mi_2_meters(10)  #in meters adjust as needed
 
+
 def get_results_from_api(location, radius, place_type):
+    c = 0
     for keyword in places_to_add[place_type]:
         # Create a list to store result types
         results_list = [] 
         next_page_token = None
+
+        print('Gathering results...',keyword,location)
 
         # Call API to store results by passing in parameters
         response = map_client.places_nearby(
@@ -127,11 +132,10 @@ def get_results_from_api(location, radius, place_type):
             keyword=keyword,
         ) 
 
-        
         # Append results to the results list adding the main type as a category
         for item in response.get('results'):
-            if place_type == 'Nightlife': item['Age'] = 'Y'
-            else: item['Age'] = 'N'
+            if place_type == 'Nightlife': item['age'] = 'Y'
+            else: item['age'] = 'N'
 
 
             tempSubtypes = []
@@ -140,36 +144,41 @@ def get_results_from_api(location, radius, place_type):
                     tempSubtypes.append(subtypes_mapping[sub_type])
                 elif sub_type in removal_mapping:
                     continue
+            
+            if keyword not in tempSubtypes:
+                tempSubtypes.append(keyword)
 
             item['sub_types']=tempSubtypes
             item['main_type']=place_type
             results_list.append(item)
 
-
+        c+=1
         next_page_token = response.get('next_page_token')
 
         # The following allows us to continuously get results.
         # It works like a Google search with results per pages.
         # Thus, we have to sequentially go through pages and continue
         # To store results
-        while next_page_token:
+        while next_page_token and c < 3:
             #Have to sleep for api requests
             time.sleep(2)   
 
             #Just for visuals sake...
-            print('Gathering results...')
+            print('Gathering results...',keyword,location)
 
             response = map_client.places_nearby(
                 location=location, 
                 radius=radius,
                 keyword=keyword,  # Include multiple keywords
                 page_token=next_page_token,
-            ) 
+            )
+            c+=1
+
 
             # Append results to the results list adding the main type as a category
         for item in response.get('results'):
-            if place_type == 'Nightlife': item['Age'] = 'Y'
-            else: item['Age'] = 'N'
+            if place_type == 'Nightlife': item['age'] = 'Y'
+            else: item['age'] = 'N'
 
 
             tempSubtypes = []
@@ -179,12 +188,15 @@ def get_results_from_api(location, radius, place_type):
                 elif sub_type in removal_mapping:
                     continue
 
+            if keyword not in tempSubtypes:
+                tempSubtypes.append(keyword)
+                
             item['sub_types']=tempSubtypes
             item['main_type']=place_type
             results_list.append(item)
 
             next_page_token = response.get('next_page_token') if 'next_page_token' in response else None  # Update next_page_token for the next iteration
-            
+        
         return results_list    #Create a list to store result types
    
 ################################################################
@@ -201,38 +213,68 @@ for types in places_to_add:
 places_df = pd.DataFrame(places_list)
 
 #Extract Latitude and longitude and rename the address
-places_df['latitude'] = places_df['geometry'].apply(lambda x: x['location']['lat'])
-places_df['longitude'] = places_df['geometry'].apply(lambda x: x['location']['lng'])
+places_df['lat'] = places_df['geometry'].apply(lambda x: x['location']['lat'])
+places_df['lon'] = places_df['geometry'].apply(lambda x: x['location']['lng'])
 places_df['google_address'] = 'https://www.google.com/maps/place/?q=place_id:' + places_df['place_id']
-places_df.rename(columns= {'vicinity':'address'})
+places_df.rename(columns= {'vicinity':'address'},inplace=True)
 
 #Drop irrelevant or non-uniform data
 columns_to_drop = ['icon','icon_mask_base_uri','business_status','icon_background_color',
                    'opening_hours', 'scope', 'plus_code','place_id','geometry','permanently_closed'] 
 
-for column in columns_to_drop:
-    places_df = places_df.drop(column, axis=1)
+places_df = places_df.drop(columns=columns_to_drop)
 
 ################################################################
 
 # Print the DataFrame
-excel_filename = "places_data.xlsx"
-places_df.to_excel(excel_filename, index=False)
+#excel_filename = "places_data.xlsx"
+#places_df.to_excel(excel_filename, index=False)
 
-print(f"Data has been saved to {excel_filename}")
+#
+# print(f"Data has been saved to {excel_filename}")
 
 
-#For inserting into database, use names specified by the database schema
-data_to_insert = {
-    "name": '',
-    "lat":'',
-    "lon": '',
-    "address": '',
-    "main_type": '',
-    "sub_types": '',
-    "rating": '',
-    "rating_amount": '',
-    "age": '',
-    #"from": 'google', #might be useful to know where its from, no useability other than knowing.
-    "weblink": '',
-}
+################################################################################################
+#MONGO DB STUFF
+from pymongo import MongoClient, ASCENDING, errors
+
+connection_string = "mongodb+srv://hangodb:hangodb@cluster0.phdgtft.mongodb.net/"
+dbname = "Hango"
+collection_name = "Places"
+
+# Establish a connection to MongoDB Atlas
+client = MongoClient(connection_string)
+
+# Accessing the database
+db = client[dbname]
+
+# Accessing the collection
+collection = db[collection_name]
+
+#If for some reason we have duplicates in our data we want to make sure they only appear once in the database
+collection.create_index([("name", ASCENDING), ("address", ASCENDING)], unique=True)
+
+
+for index, row in places_df.iterrows():
+    #For inserting into database, use names specified by the database schema
+    data_to_insert = {
+        "name": row['name'],
+        "lat": row['lat'],
+        "lon": row['lon'],
+        "address": row['address'],
+        "main_type": row['main_type'],
+        "sub_types": row['sub_types'],
+        "rating": row['rating'],
+        "rating_amount": row['user_ratings_total'],
+        "age": row['age'],
+        "price": row['price_level'],
+        #"from": 'google', #might be useful to know where its from, no useability other than knowing.
+        "weblink": row['google_address'],
+    }
+
+    try: #running into a error that prevents inserts from proceeding if duplicates so we have to catch the errors.
+        result = collection.insert_one(data_to_insert)
+        print(f"Insert successful for {row['name']}")
+    except errors.DuplicateKeyError:
+        print(f"Duplicate key error. Skipping insertion for {row['name']}")
+        continue

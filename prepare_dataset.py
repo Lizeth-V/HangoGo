0c213c2cf64328737136ea736a5b79b81406b80e
aidan-ma316 <77308659+aidan-ma316@users.noreply.github.com>
2024-02-17 16:46:23 -0800

    Update prepare_dataset.py
    This is the prototype just in python file

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -78,3 +78,66 @@ uid = '6567dcefba91df16f20f718d'
 generated_reviews = generateFakeReviews(id=uid, age=25)
 
 generated_reviews.head(100)
+
+#merge the two dataframes based on the place id to generate factor matrix
+#drop features that are irrelevant to the model (distinction)
+merged_db = pd.merge(generated_reviews, df, left_on='place', right_on='place_id', how='inner')
+merged_db = merged_db.drop(columns=['user_id','place_id','place', 'name', 'address', 'weblink','price'])
+
+#For now I think, we might be overfitting with every amount of data
+merged_db['sub_types'] = merged_db['sub_types'].apply(lambda x: x[:3] if isinstance(x, list) else x)
+merged_db[['subtype_1', 'subtype_2', 'subtype_3']] = pd.DataFrame(merged_db['sub_types'].tolist(), index=merged_db.index)
+
+# Drop the original 'sub_types' column if needed
+merged_db = merged_db.drop(columns=['sub_types'])
+    
+# Display the combined dataset
+print(merged_db.head())
+print(merged_db.size)
+
+#turn into datasets
+
+#divide the data in 2 sets train and validation
+val_dataframe = merged_db.sample(frac=0.2, random_state=1337)
+train_dataframe = merged_db.drop(val_dataframe.index)
+
+#divide into features and the output value
+val_ds_y = val_dataframe['feedback']
+train_ds_y = train_dataframe['feedback']
+
+val_ds_x = val_dataframe.drop(['feedback'], axis=1)
+train_ds_x = train_dataframe.drop('feedback', axis=1)
+
+
+
+#pardon if this is changed in the future this is only a month old not much documentation on it ;-;
+feature_space = FeatureSpace(
+    features={
+        
+        #"price": "integer_categorical", ##NEEDS TO BE PREPROCESSED SOMETIMES DICT SOMETIMES NOT
+        
+        "main_type": "string_categorical",
+        "sub_type_1": "string_categorical",
+        "sub_type_2": "string_categorical",
+        "sub_type_3": "string_categorical",
+        "age": "string_categorical",
+        
+        #"u_age": "float_discretized",
+        #I'm not sure if we can even use age to predict.
+        
+        #normalized stuff
+        "lat": "float_normalized",
+        "lon": "float_normalized",
+        "rating": "float_normalized",
+        "rating_amount": "float_normalized",
+    },
+    #We make features that depend on both im not certain how reliable this will be.
+    crosses=[("lat", "lon"), ("rating", "rating_amount")],
+    crossing_dim=32,
+    
+    output_mode="concat",
+)
+
+
+#Here are our test and validation sets ^
+#Build the Model Below

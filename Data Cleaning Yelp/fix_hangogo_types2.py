e54d3be34527ec877163686e2b44ef9df4924278
Nhu <nqnhu04@gmail.com>
2024-02-22 14:13:11 -0800

    Fix subtypes so no plurals

--- Data Cleaning Yelp/fix_hangogo_types2.py
+++ Data Cleaning Yelp/fix_hangogo_types2.py
@@ -9,8 +9,7 @@ with open('Hango.Places.json', encoding='utf-8') as f:
     for place in places:
         for i in range(len(place['sub_types'])):
             if place['sub_types'][i] == 'amusement_park' or place['sub_types'][i] == 'amusement_parks':
-                place['sub_types'][i] == 'amusement_park'
-                print(place['sub_types'][i])
+                place['sub_types'][i] = 'amusement_park'
             if place['sub_types'][i] == 'art_gallery' or place['sub_types'][i] == 'art_galleries':
                 place['sub_types'][i] = 'art_gallery'
             if place['sub_types'][i] == 'bakery' or place['sub_types'][i] == 'bakeries':

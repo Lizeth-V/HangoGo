10c9966e3b53eec25d00c76043442dd4caafd250
aidan-ma316 <77308659+aidan-ma316@users.noreply.github.com>
2024-03-05 20:09:17 -0800

    Update generate_images_link.py

--- generate_images_link.py
+++ generate_images_link.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from bson import ObjectId
 from selenium import webdriver
+import dda
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 
 # MongoDB connection details
@@ -17,50 +19,47 @@ db = client[dbname]
 collection = db[collection_name]
 
 # Function to scrape the first picture for each place with a weblink
-def scrape_images():
-    # Iterate through documents in the collection
-    for document in list(collection.find({"weblink": {"$exists": True}})):
-        weblink = document["weblink"]
+def scrape_images(doc):
+        weblink = doc["weblink"]
         
-       # Assuming you have the ChromeDriver executable in your PATH
         driver = webdriver.Chrome()
 
-        # Replace 'your_weblink' with the actual weblink
         driver.get(weblink)
 
-        # Get the page source after waiting for a few seconds (adjust the sleep time as needed)
-        time.sleep(5)
+        time.sleep(1)
         page_source = driver.page_source
 
-        # Parse the HTML content
         soup = BeautifulSoup(page_source, 'html.parser')
 
-        # Find the first button tag with the specified class
         button_tag = soup.find('button', class_='aoRNLd kn2E5e NMjTrf lvtCsd')
 
         print(button_tag)
 
-        # Close the Selenium driver
         driver.quit()
 
         if button_tag:
             img_tag = button_tag.find('img')
             image_url = img_tag.get('src')
 
-            # Print or save the image URL as needed
-            print(f"Place: {document['name']}, Image URL: {image_url}")
+            print(f"Place: {doc['name']}, Image URL: {image_url}")
 
-            # Update the document with the image URL
-            collection.update_one(
-                {"_id": document["_id"]},
-                {"$set": {"image_url": image_url}},
-                upsert=True
-            )
+            if image_url:
+                collection.update_one(
+                    {"_id": doc["_id"]},
+                    {"$set": {"image_url": image_url}},
+                    upsert=True
+                )
 
-                #print(image_url)
+            else: print("No Visiible Picture")
 
-# Run the scraping function
-scrape_images()
+def multithread_scrape():
+    l = list(collection.find({{"weblink": {"$exists": True}, "image_url": {"$exists": False}}}))
 
-# Close the MongoDB connection
+    num_threads = 4
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        executor.map(scrape_images, l)
+
+    
+
+multithread_scrape()
 client.close()

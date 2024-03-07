ebcd46d0d73c0a16435f44f7259d468390170467
aidan-ma316 <77308659+aidan-ma316@users.noreply.github.com>
2024-03-07 14:33:02 -0800

    rename

--- cool.py
+++ cool.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
-import generate_model
 import temp_feedback
 import return_highest_rec as retH
-
 
 app = Flask(__name__)
 
@@ -10,10 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/get_place_details')
 def get_active_place_details(user_id=None):
-    user_id = '6568cbef4a9658311b3ee704'  # Replace with actual user ID
+    user_id = '6568cbef4a9658311b3ee704'  #test id
     active_place = retH.match_highest_list(retH.get_highest_list(user_id), radius = 500, place_type = None)
     
     return render_template('index.html', active_place=active_place)
@@ -21,23 +18,24 @@ def get_active_place_details(user_id=None):
 
 @app.route('/accept_rec/<user_id>/<place_id>', methods=['POST'])
 def accept_rec_model(user_id=None, place_id=None):
+    import generate_model
 
-    user_id = '6568cbef4a9658311b3ee704'  # Replace with actual user ID
+    user_id = '6568cbef4a9658311b3ee704'  #temp
     temp_feedback.accept_recommendation_update(user_id=user_id,place_id=place_id)
     generate_model.generate_place_probabilities(user_id)
 
-
-    return render_template('result.html')  # You can customize this template as needed
+    return render_template('result.html')  #html for after
 
 @app.route('/decline_rec/<user_id>/<place_id>', methods=['POST'])
 def decline_rec_model(user_id=None, place_id=None):
+    import generate_model
+
     
-    user_id = '6568cbef4a9658311b3ee704'  # Replace with actual user ID
+    user_id = '6568cbef4a9658311b3ee704'  #\test id
     temp_feedback.decline_recommendation_update(user_id=user_id, place_id=place_id)
     generate_model.generate_place_probabilities(user_id)
 
-
-    return render_template('result.html')  # You can customize this template as needed'''
+    return render_template('result.html')  #
 
 if __name__ == '__main__':
     app.run(debug=True)
--- temp.py
+++ temp.py
@@ -1,43 +1,37 @@
 from pymongo import MongoClient
 from bson import ObjectId
 
-# MongoDB connection details
+#mongo
 connection_string = "mongodb+srv://hangodb:hangodb@cluster0.phdgtft.mongodb.net/"
 dbname = "Hango"
 collection_name = "Places"
 
-# Connect to MongoDB
 client = MongoClient(connection_string)
 db = client[dbname]
 collection = db[collection_name]
 
-# Function to update documents
+#update the collection with a lcoation field so i can do radius in the return
 def update_documents():
-    # Iterate through documents in the collection
     for document in collection.find():
-        # Extract lat and lon values
         lat = document.get("lat")
         lon = document.get("lon")
 
-        # Create a new location field with 2dsphere index structure
+        #new field
         location_field = {
             "type": "Point",
             "coordinates": [lon, lat]
         }
 
-        # Update the document with the new location field
+        #update every doc
         collection.update_one(
             {"_id": document["_id"]},
             {"$set": {"location": location_field}}
         )
 
-# Create a 2dsphere index on the location field
 collection.create_index([("location", "2dsphere")])
 
-# Update documents
 update_documents()
 
-# Close the MongoDB connection
 client.close()
 
 print("Documents updated successfully.")

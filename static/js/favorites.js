5be8bc7df8f18e0c4b701588d2c72bcf854fefe8
Gloria Lee <gloria.lee02@student.csulb.edu>
2024-04-06 21:24:33 -0700

    fixed UI scalability for favorites

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -1,26 +1,9 @@
-function expandChatbox() {
-    const favoritesList = document.querySelector('.favorites-list');
-    const userChatbox = document.querySelector('.user-chatbox');
-    favoritesList.classList.toggle('hide-favorites');
-    userChatbox.classList.toggle('expanded-chatbox');
-} 
-
-function expandFavorites() {
-    const favoritesList = document.querySelector('.favorites-list');
-    const userChatbox = document.querySelector('.user-chatbox');
-    favoritesList.classList.toggle('expanded-favorites');
-    userChatbox.classList.toggle('hide-chatbox');
+function openNav() {
+    document.getElementById("mySidebar").style.width = "50vw";
+    document.getElementById("main").style.marginLeft = "250px";
 }
-
-function addToFavorites(index) {
-$.ajax({
-    url: '/add_to_favorites',
-    type: 'POST',
-    data: { place_index: index },
-    success: function (response) {
-    if (response.success) {
-        alert(response.message)
-    }
-}
-});
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
 }

3ba77ee2faf5ef8be64693347ac72b2624e51d8d
aidan-ma316 <77308659+aidan-ma316@users.noreply.github.com>
2024-05-05 17:07:05 -0700

    Update

--- temp_feedback.py
+++ temp_feedback.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient, ASCENDING
 import time
 import datetime
+from bson import ObjectId
 
 
 connection_string = "mongodb+srv://hangodb:hangodb@cluster0.phdgtft.mongodb.net/"
@@ -37,7 +38,10 @@ def accept_recommendation_update(user_id, place_id):
     else:
         print("Update successful. Matched document:", update_result.matched_count)
 
+    set_active_place(user_id, place_id)
+
     client.close()
+
 
 
 def add_to_favorites_update(user_id, place_id):
@@ -59,7 +63,7 @@ def add_to_favorites_update(user_id, place_id):
         "user_id": user_id,
         "place_id": place_id,
         "timestamp": datetime.datetime.utcnow(),
-        "feedback": 1
+        "feedback": 2
     }
 
     update_result = collection.update_one(criteria, {"$set": data_to_insert}, upsert=True)
@@ -70,6 +74,92 @@ def add_to_favorites_update(user_id, place_id):
         print("Update successful. Matched document:", update_result.matched_count)
 
     client.close()
+
+
+def set_active_place(user_id, place_id):
+    try:
+        client = MongoClient(connection_string)
+        db = client[dbname]
+        collection = db['User Data']
+
+        criteria = {"_id": ObjectId(user_id)}
+        data_to_insert = {"active_place": place_id}
+
+        print(criteria)
+
+        update_result = collection.update_one(criteria, {"$set": data_to_insert}, upsert=True)
+
+        if update_result.upserted_id:
+            print("Insertion successful:", update_result.upserted_id)
+        else:
+            print("Update successful. Matched document:", update_result.matched_count)
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        client.close()
+
+def get_active_place(user_id):
+    try:
+        client = MongoClient(connection_string)
+        db = client[dbname]
+        collection = db['User Data']
+
+        criteria = {"_id": ObjectId(user_id)}
+        projection = {"active_place": 1, "_id": 0}
+        document = collection.find_one(criteria, projection)
+
+        if document:
+            return get_place_details(document['active_place'])
+        else:
+            print("User not found")
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        client.close()
+
+def remove_active_place(user_id):
+    try:
+        client = MongoClient(connection_string)
+        db = client[dbname]
+        collection = db['User Data']
+
+        criteria = {"_id": ObjectId(user_id)}
+
+        update_result = collection.update_one(criteria, {"$unset": {"active_place": ""}})
+
+        if update_result.modified_count > 0:
+            print("Active place removed successfully.")
+            return True
+        else:
+            print("No active place was found or user does not exist.")
+            return False
+
+    except Exception as e:
+        print("An error occurred:", e)
+        return False
+
+    finally:
+        # Ensure the MongoDB client is closed
+        client.close()
+
+def get_place_details(place_id):
+    try:
+        client = MongoClient(connection_string)
+        db = client[dbname]
+        collection = db['Places']
+
+        criteria = {"_id": ObjectId(place_id)}
+        projection = {"name": 1, "lat":1, "lon": 1, "address":1, "_id": 1}
+        document = collection.find_one(criteria, projection)
+        
+        if document:
+            return document
+        else:
+            print("User not found")
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        client.close()
 
 
 
@@ -92,7 +182,7 @@ def decline_recommendation_update(user_id, place_id):
         "user_id": user_id,
         "place_id": place_id,
         "timestamp": datetime.datetime.utcnow(),
-        "feedback": 0
+        "feedback": -0.5
     }
 
     update_result = collection.update_one(criteria, {"$set": data_to_insert}, upsert=True)
@@ -123,7 +213,7 @@ def block_recommendation_update(user_id, place_id):
         "user_id": user_id,
         "place_id": place_id,
         "timestamp": datetime.datetime.utcnow(),
-        "feedback": 0
+        "feedback": -1
     }
 
     update_result = collection.update_one(criteria, {"$set": data_to_insert}, upsert=True)

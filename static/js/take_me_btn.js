be0e54871d83cd04a00b1d90965549b34539768d
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2024-05-09 13:07:47 -0700

    js for the 'take me there' button no longer embeded

---
+++ static/js/take_me_btn.js
@@ -0,0 +1,14 @@
+function setActivePlace(user_id, place_id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/set_active_place_route', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+            // Handle response here, e.g., show a success message
+        }
+    };
+    var data = JSON.stringify({user_id: user_id, place_id: place_id});
+    xhr.send(data);
+}
